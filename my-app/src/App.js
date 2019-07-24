c94da4e42df6da87c418970543a101aefb698339
Alejandro Sebastian Haro Munoz <alejandro@ip-10-164-124-190.ec2.internal>
2019-07-24 16:27:10 -0400

    Added logic to render an array of artists

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function Hero(){
+  return(<div className="row">
+    <div className="jumbotron col-10 offset-1">
+      <h1> Author Quiz </h1>
+      <p> Select the song written by the author shown</p>
     </div>
-  );
+  </div>);
 }
 
-export default App;
+function Artist({title}){
+  return(<div className="answer">
+    <h4>
+      {title}
+    </h4>
+  </div>)
+}
+
+function Turn({author,songs}){
+  return (<div className="row turn" style={{backgroundColor:"White"}}>
+      <div className="col-4 offset-1">
+      <img style={{width: 400, height: 600}} src={author.imageUrl} className="authorimage" alt="Author"/>
+      </div>
+      <div className="col-6 offset-1">
+        {songs.map((title) => <Artist title={title} key={title}/>)}
+      </div>
+      
+      </div>);
+}
+
+function Continue(){
+  return (<div/>)
+}
+
+function Footer(){
+  return(<div id="footer">
+    <div className="col-12">
+      <p className="text-muted credit"> All images are from Wikimedia Commons </p>
+    </div>
+  </div>);
+}
+
+function AuthorQuiz({turnData}){
+    return (
+      <div className="container-fluid">
+        <Hero/>
+        <Turn {...turnData}/>
+        <Continue/>
+        <Footer/>
+      </div>
+    );
+}
+
+export default AuthorQuiz;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AuthorQuiz from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const authors = [
+    {
+    name: 'James Hetfield',
+    imageUrl: 'images/authors/jameshetfield.jpg',
+    imageSource:'Wikimedia Commons',
+    songs:['Master of Puppets']
+    }
+];
 
+const state ={
+    turnData:{
+    author: authors[0],
+    songs: authors[0].songs
+    }
+}
+
+ReactDOM.render(<AuthorQuiz {...state} />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA

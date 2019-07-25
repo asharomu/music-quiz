aeffb353aecc551a0cf67c43f9b311540b3ad27c
asharomu <25918532+asharomu@users.noreply.github.com>
2019-07-24 19:46:00 -0500

    Update index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import AuthorQuiz from './App';
 import * as serviceWorker from './serviceWorker';
+import {shuffle,sample} from'underscore'
 
 const authors = [
     {
@@ -27,11 +28,24 @@ const authors = [
     }
 ];
 
-const state ={
-    turnData:{
-    author: authors[2],
-    songs: authors[2].songs
+function getTurnData(authors) {
+    const allSongs = authors.reduce(function(p,c,i) {
+        return p.concat(c.songs);
+    }, []);
+    
+    const fourRandomSongs = shuffle(allSongs).slice(0,4);
+    const answer = sample(fourRandomSongs);
+
+    return {
+        songs: fourRandomSongs,
+        author: authors.find((author) =>
+            author.songs.some((title) => 
+            title === answer))    
     }
+}
+
+const state ={
+    turnData:getTurnData(authors)
 }
 
 ReactDOM.render(<AuthorQuiz {...state} />, document.getElementById('root'));
